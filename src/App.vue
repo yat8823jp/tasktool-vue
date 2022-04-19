043907f45522b1c3dfd01f9b0c1b5f822ad4e6a9
YAT <yat@yat-net.com>
2022-04-19 18:32:51 +0900

    -save-restore

--- src/App.vue
+++ src/App.vue
@@ -35,6 +35,9 @@
 				フィルタしない
 			</li>
 		</ul>
+		<h2>保存と復元</h2>
+		<button type="button" v-on:click="save">保存</button>
+		<button type="button" v-on:click="restore">復元</button>
 	</div>
 </template>
 
@@ -97,6 +100,16 @@ export default {
 				filter: labelId
 			} )
 		},
+		//現在の状態を保存する
+		save() {
+			//'sava' アクションをコミット
+			this.$store.dispatch( 'save' )
+		},
+		//保存されている状態を復元する
+		restore() {
+			//'restore アクションをコミット
+			this.$store.dispatch( 'restore' )
+		}
 	}
 }
 </script>
